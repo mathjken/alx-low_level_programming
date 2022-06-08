183ac829ab7287a91e363f711e23b6a0b57c460d
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-06-07 17:45:02 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Return: 1 if lower or uppercase, 0 if not alphabet character
+ */
+
+int _isalpha(int chr)
+{
+	if ((chr >= 'a' && char <= 'z') || (chr >= 'A' && chr <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
