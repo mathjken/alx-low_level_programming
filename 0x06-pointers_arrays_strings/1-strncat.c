c061714edb35711121c86c2601b2e72d61f0a1a8
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-06-15 13:14:26 -0700

    fixed header error

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,23 @@
 #include "main.h"
 
-#include "holberton.h"
-
 /**
- * _strncat - concatenate n bytes to destination string
- * @dest: string to be appended to
- * @src: string to append
- * @n: append n number of bytes(chars)
- * Return: concatenated string
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i] != '\0') /*i is index of null terminator*/
-		i++;
+	while (dest[index++])
+		dest_len++;
 
-	while (src[j] != src[n]) /*append replacing null terminator*/
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
-
