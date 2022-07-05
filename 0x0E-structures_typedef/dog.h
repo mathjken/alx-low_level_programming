0e964007139f7f49cf125892294b77be433a922d
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-07-05 13:36:44 -0700

    new tes commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * dog_t - Typedef for struct dog
+/*
+ * File: dog.h
+ * Desc: header file section.
  */
-typedef struct dog dog_t;
 
 /**
- * struct dog - structure for dogs
- * @name: first member
- * @age: second member
- * @owner: third member
+ * struct dog - A new type describing a dog.
+ * @name: defines the name of the dog.
+ * @age: defines the age of the dog.
+ * @owner: specifies the owner of the dog.
  */
 struct dog
 {
@@ -18,6 +18,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
