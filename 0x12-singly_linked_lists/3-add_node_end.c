6d6a741198d40c0fcf3f1bc2c13ac9649d835dcf
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-07-14 17:03:18 -0700

    new commit for task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,69 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
-  * *add_node_end - adds a new node at the end of a linked list.
-  * @head: start of the list.
-  * @str: string.
-  * Return: the address of the new element,
-  * or null if it failed.
-  */
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * _strdup - recreation of string duplicate function
+ * @src: source of string to duplicate
+ * Return: pointer to malloc'd space with copied string
+ */
+void *_strdup(const char *src)
+{
+	int len, i;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * add_node_end - add new nodes to the end of the list
+ * @head: current place in the list
+ * @str: string to add to the head
+ * Return: pointer to current position in list
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *new, *current;
+	char *dupstr;
 
-	newNode = (list_t *)malloc(sizeof(list_t));
-	if (newNode == NULL)
+	if (str == NULL)
 		return (NULL);
-	newNode->str = strdup(str);
-	newNode->len = string_length(str);
-	newNode->next = NULL;
-	/* check if list is null and make new node the head */
+	dupstr = _strdup(str);
+	if (dupstr == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = dupstr;
+	new->len = _strlen(str);
+	new->next = NULL;
 	if (*head == NULL)
-		*head = newNode;
-	/* else find last node and add new node */
-	else
 	{
-		list_t *lastNode = *head;
-		/* last node has to be NULL use while to check */
-		while (lastNode->next != NULL)
-		{
-			lastNode = lastNode->next;
-		}
-		/* if null is found add new node at the end */
-		lastNode->next = newNode;
+		*head = new;
+		return (*head);
 	}
-	return (newNode);
-}
-
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(const char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
+	return (*head);
 }
