1ac76b067d1e1d37732db50af9324bbd43b53b03
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-07-18 13:08:03 -0700

    new commit

--- 0x13-more_singly_linked_lists/0-print_listnt.c
+++ 0x13-more_singly_linked_lists/0-print_listnt.c
@@ -1,23 +1,18 @@
-#include "lists.h"
 #include <stdio.h>
-
+#include "lists.h"
 /**
- * print_listint - print elements of linkedlist
- * @h: linked list
- *
+ * print_listint - prints all elements of a list_t list
+ * @h: singly linked list elements
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t nbr_nodes = 0;
-	const listint_t *current = h;
+	size_t x;
 
-	while (current != NULL)
+	for (x = 0; h; x++)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
-		nbr_nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
-	return (nbr_nodes);
+	return (x);
 }
